64108788f675f86914e2d854f697acd333686288
Jair <jairaceves56@gmail.com>
2022-05-11 13:38:23 -0500

    Iniciamos la Calculadora

---
+++ src/PrimeraApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+//FC
+const PrimeraApp = () => {
+
+    return (
+        <>
+            <h1>Calculadora con React</h1>
+            <p>Mi primera Caluladora</p>
+        </>
+     
+    )
+
+}
+
+export default PrimeraApp;
